728044cbe3e1df0014793248e8de09d5f3b44fb3
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-05-25 11:47:42 +0530

    Update searching_ofarray.c

--- searching_ofarray.c
+++ searching_ofarray.c
@@ -1,4 +1,4 @@
-/*#include<stdio.h>
+#include<stdio.h>
 int main()
 {
     int i,n,arr[100],m,c=0;
@@ -24,51 +24,4 @@ int main()
 		{
 			printf("not found");
 		}
-}*/
-#include<stdio.h>
-int search(int *arr,int n,int se)
-{
-    /*int i,max=0;
-	for(i=0;i<n;i++)
-	{
-	   if(arr[i]==se)
-	   {
-	       max=i;
-	   }
-	   	
-	}
-	return max;*/
-	int static i=0;
-	if(i==n)
-	{
-		return 0;
-	}
-	if(arr[i]==se)
-	{
-		return 2;
-	}
-	i++;
-	return search(arr,n,se);
-		
-	
-}
-
-int main()
-{
-    int arr[100],n,i,se;
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&arr[i]);
-	}
-	scanf("%d",&se);
-	//printf("%d ",search(arr,n,se));
-    if(search(arr,n,se)==2)
-    {
-    	printf("found");
-	}
-	else
-	{
-		printf("not found");
-	}
 }
